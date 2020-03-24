df3885339398e033065f7f27bde60558e135c57a
Julien Renaux <julien.renaux@forgerock.com>
2020-03-24 18:50:48 +0100

    expose layout service

--- projects/layouts/main-layout/src/public_api.ts
+++ projects/layouts/main-layout/src/public_api.ts
@@ -3,4 +3,5 @@ export * from './main-layout.module';
 export * from './main-layout.component';
 export * from './main-layout.config.service';
 export * from './sidebar/sidebar.component';
+export * from './navigation/navigation.service';
 export * from './models';
